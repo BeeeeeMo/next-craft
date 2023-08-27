a5ab98137190b0dedb6a16e387e1ec3124aea154
BeeeeeMo <HI@beeeeemo.dev>
2023-08-27 20:09:20 +0000

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,15 +7,15 @@ const BLOG = {
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   craftConfigShareUrl: process.env.CRAFT_CONFIG_SHARE_URL
     ? process.env.CRAFT_CONFIG_SHARE_URL
-    : 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
+    : 'https://www.craft.me/s/f6C5DYOvtC8erB', // The link to share your craft config
   seo: {
-    keywords: ['Blog', 'Craft.do', 'Craft Docs', 'Next.js', 'TailwindCSS']
+    keywords: []
   },
   analytics: {
-    provider: '', // Currently support Umami, fill with 'umami' to enable or leave it empty to disable it.
+    provider: 'umami', // Currently support Umami, fill with 'umami' to enable or leave it empty to disable it.
     umamiConfig: {
-      scriptUrl: '', // The url of your Umami script
-      websiteId: '' // The website id of your Umami instance
+      scriptUrl: 'https://beeeeemo.dev/tr', // The url of your Umami script
+      websiteId: '062b90e8-da37-431a-b7c2-2169835f117b' // The website id of your Umami instance
     }
   },
   isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
